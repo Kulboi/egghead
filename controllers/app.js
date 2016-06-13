c5f2e968c4d3e5f180fa1dd3242305efc7205695
unknown <Habeeb Mac-iver>
2016-06-13 07:57:43 +0100

    some major changes

--- controllers/app.js
+++ controllers/app.js
@@ -5,7 +5,7 @@ var app =  angular.module("App-name",['ngRoute']);
 
 app.run(function($rootScope){
   $rootScope.BaseServer = 'http://localhost/project-root' ; //to absolute path to project folder
-
+});
 
 app.config( ['$routeProvider', function($routeProvider) {
 
